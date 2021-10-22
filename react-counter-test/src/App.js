cd47634fc6a4041cf354c87f0eba4c59d72c6c34
seanpeak-dev <seanpeak.dev@gmail.com>
2021-10-22 15:40:48 +0900

    Feat: 간단한 카운터 앱으로 테스트 연습

--- react-counter-test/src/App.js
+++ react-counter-test/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button
+            data-testid="plus-btn"
+            onClick={() => setCounter((counter) => counter + 1)}
+            disabled={disabled}
+          >
+            +
+          </button>
+          <button
+            data-testid="minus-btn"
+            onClick={() => setCounter((counter) => counter - 1)}
+            disabled={disabled}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on-off-btn"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => {
+              setDisabled((disabled) => !disabled);
+            }}
+          >
+            on | off
+          </button>
+        </div>
       </header>
     </div>
   );
--- react-counter-test/src/App.test.js
+++ react-counter-test/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterEl = screen.getByTestId("counter");
+  expect(counterEl).toHaveTextContent(0);
+});
+
+test("when the plus-btn is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const plusBtnEl = screen.getByTestId("plus-btn");
+  fireEvent.click(plusBtnEl);
+  const counterEl = screen.getByTestId("counter");
+  expect(counterEl).toHaveTextContent(1);
+});
+
+test("on-off btn has a blue color", () => {
+  render(<App />);
+  const onOffBtnEl = screen.getByTestId("on-off-btn");
+  expect(onOffBtnEl).toHaveStyle({ backgroundColor: "blue" });
+});
+
+test("prevent the plus and minus btn from being pressed when the on-off-btn is clicked", () => {
+  render(<App />);
+  const onOffBtnEl = screen.getByTestId("on-off-btn");
+  fireEvent.click(onOffBtnEl);
+  const plusBtnEl = screen.getByTestId("plus-btn");
+  expect(plusBtnEl).toBeDisabled();
 });
